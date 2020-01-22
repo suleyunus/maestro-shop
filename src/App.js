663de8addf6fc4bfb614711ddfb5675c8278a9fd
Suleiman Yunus <kayangasuleiman@gmail.com>
2020-01-22 17:52:41 +0300

    add basic routing logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch> 
+        <Route exact path='/' component={ HomePage }/>
+        <Route exact path='/hats' component={ HatsPage }/>
+      </Switch>
     </div>
   );
 }
